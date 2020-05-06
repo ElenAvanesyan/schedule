9efcb56f053176eb29b60cd5ed70a4a3e103cd0d
Elen Avanesyan <eavanesyan@macadamian.com>
2020-05-07 00:51:31 +0400

    Capacity query

--- src/main/java/com/schedule/suggestion/persistence/repositories/StudentRepository.java
+++ src/main/java/com/schedule/suggestion/persistence/repositories/StudentRepository.java
@@ -16,12 +16,14 @@ public interface StudentRepository extends JpaRepository<Student, Long>{
     @Query("select distinct s.listOfCourse from Student s where s.username=:username")
     List<Course> getStudentPassedCourses(@Param("username") String username);
 
-    @Query(value="select count(s.id) from student s where " +
-            "(select count(req.prerequisite_course_id) from course crs left join course_prerequisite req " +
+    @Query(value="select count(st.id) from Student st where " +
+            "(select count(req.prerequisite_course_id) from Course crs left join course_prerequisite req " +
             "on req.course_id = crs.id where crs.id=:courseId and (req.course_id is null or req.prerequisite_course_id in " +
-            "(select crs2.id from completion comp join course crs2 on crs2.id = comp.course_id where comp.student_id = s.id))) = " +
-            "(select count(req2.prerequisite_course_id) from course crs3 left join course_prerequisite req2 on req2.course_id = crs3.id " +
-            "where crs3.id=:courseId)",
+            "(select crs2.id from completion comp join course crs2 on crs2.id = comp.course_id where comp.student_id = st.id) and " +
+            "(select comp2.grade from completion comp2 join course crs4 on crs4.id = comp2.course_id where comp2.student_id = st.id and comp2.course_id = req.prerequisite_course_id) > 0)) = " +
+            "(select count(req2.prerequisite_course_id) from Course crs3 left join course_prerequisite req2 on req2.course_id = crs3.id where crs3.id=:courseId) and " +
+            "((select comp3.course_id from completion comp3 where comp3.student_id = st.id and comp3.course_id=:courseId) is null or " +
+            "(select comp4.grade from completion comp4 where comp4.student_id = st.id and comp4.course_id=:courseId) = 0)",
             nativeQuery = true)
     Integer findMaximumCourseCapacity(@Param("courseId") Integer courseId);
 }
